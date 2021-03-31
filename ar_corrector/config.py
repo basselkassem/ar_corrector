30496c9f1691c00171517c50b5e240d8e74a74a6
basselkassem <bassel.sh.k@gmail.com>
2021-03-31 06:24:47 +0000

    change the path

--- ar_corrector/config.py
+++ ar_corrector/config.py
@@ -1,3 +1,8 @@
+#%%
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
 config = {
-    'vocabs_dict': 'ar_corrector/resources/vocabs.pickle'
+    'vocabs_dict': dir_path + '/resources/vocabs.pickle'
 }
+print(config)
+# %%
